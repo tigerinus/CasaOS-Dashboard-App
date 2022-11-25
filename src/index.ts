dac680433225c4fbae51a5209d594bc200c1fad5
Tiger Wang <tigerwang@outlook.com>
2022-11-24 21:14:28 -0500

    wip

--- src/index.ts
+++ src/index.ts
@@ -16,12 +16,14 @@ import dotenv from 'dotenv';
 import express, { Request, Response } from 'express'; // 导入express模块
 import http from 'http'; // 导入http模块
 import { Server } from "socket.io"; // 导入socket.io模块
+import WebSocket from 'ws'; // 导入ws模块
 
 dotenv.config();
 
 const app = express(); // 创建express实例
 const server = http.createServer(app); // 创建http服务
 const io = new Server(server); // 创建socket.io服务
+const ws = new WebSocket("wss://demo.piesocket.com/v3/channel_123?api_key=VCXCEuvhGcBDP7XhiJJUDvR1e1D3eiVjgZ9VRiaV&notify_self"); // 创建ws服务
 
 app.use(express.static('public')); // 静态资源托管
 
@@ -33,9 +35,26 @@ app.get('/', (req: Request, res: Response) => {
 // Socket.io 事件
 io.on('connection', (socket) => {
     console.log('a user connected');
+
     socket.on('disconnect', () => {
         console.log('user disconnected');
     });
+
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+        ws.send(msg);
+    });
+});
+
+// WebSocket 事件
+ws.on('open', function open() {
+    console.log('connected');
+    ws.send('something');
+});
+
+ws.on('message', function incoming(data) {
+    console.log(data.toString());
+    io.emit('chat message', data);
 });
 
 // 启动express服务
